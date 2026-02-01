8bdc1b851db4e0e1cbecd11529d108d416e13e65
seop <useop0821@gmail.com>
2026-02-01 21:44:00 +0900

    feat: WebMvcConfig 구현

---
+++ shiritori-backend/src/main/java/hello/shiritori/global/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package hello.shiritori.global.config;
+
+import hello.shiritori.global.interceptor.RateLimitInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/games/**/turn");
+    }
+
+}
