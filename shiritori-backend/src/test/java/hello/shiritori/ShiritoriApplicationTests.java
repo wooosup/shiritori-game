3f61b466e7045d791cb7203b93205ecc02e8e7cc
seop <useop0821@gmail.com>
2026-02-01 21:43:09 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ shiritori-backend/src/test/java/hello/shiritori/ShiritoriApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.shiritori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiritoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
