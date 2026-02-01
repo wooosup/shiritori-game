dcd0a3ca9910d11b62af66d3aaae85fbf5b4fd3b
seop <useop0821@gmail.com>
2026-02-01 20:18:03 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ shiritori-backend/src/main/java/hello/shiritori/ShiritoriApplication.java
@@ -0,0 +1,13 @@
+package hello.shiritori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiritoriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiritoriApplication.class, args);
+    }
+
+}
