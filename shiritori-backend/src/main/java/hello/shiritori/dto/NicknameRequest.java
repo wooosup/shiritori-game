5437a38ac809bb82b1d8829553252b7ccb500ff6
seop <useop0821@gmail.com>
2026-02-01 21:27:23 +0900

    feat: add dto

---
+++ shiritori-backend/src/main/java/hello/shiritori/dto/NicknameRequest.java
@@ -0,0 +1,13 @@
+package hello.shiritori.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NicknameRequest {
+
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+}
